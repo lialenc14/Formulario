a8d075dd6adb12890702cb096af886633c2798a6
Livia <109006719+lialenc14@users.noreply.github.com>
2023-07-20 17:51:33 -0300

    Add files via upload

---
+++ upload.svelte
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<svelte:head>
+    <title>Visualizar Respostas</title>
+</svelte:head>
+<body>
+    <input type="file" id="fileInput" accept=".json">
+    <button id="handleFile()">Carregar Arquivo JSON</button>
+    <div id="dataDisplay"></div>
+    <button id="showDataAsText()">Exibir Dados como Texto</button>
+
+<script>
+let jsonData; // Variável global para armazenar os dados do arquivo JSON
+
+function handleFile() {
+    const fileInput = document.getElementById('fileInput');
+
+    const file = fileInput.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function(event) {
+        jsonData = JSON.parse(event.target.result);
+        alert('Arquivo JSON carregado com sucesso!');
+    }
+
+    reader.readAsText(file);
+}
+
+function showDataAsText() {
+    if (!jsonData) {
+        alert('Por favor, carregue o arquivo JSON primeiro.');
+        return;
+    }
+
+    const dataDisplay = document.getElementById('dataDisplay');
+    dataDisplay.innerHTML = `<pre>${JSON.stringify(jsonData, null, 2)}</pre>`;
+}
+</script>
+</body>
